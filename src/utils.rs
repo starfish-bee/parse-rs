0b4d4ccf994a855cd796b56fee5953f06e46c54b
starfish-bee <77985573+starfish-bee@users.noreply.github.com>
2021-02-12 19:30:22 +0000

    change take_while to work with multi-byte chars

--- src/utils.rs
+++ src/utils.rs
@@ -3,10 +3,12 @@ pub fn take_while<P>(input: &str, predicate: P) -> (&str, &str)
 where
     P: Fn(char) -> bool,
 {
-    let index = match input.chars().position(|x| !predicate(x)) {
-        Some(i) => i,
-        None => input.chars().count(),
+    input.chars().for_each(|x| println!("{}", x));
+    let index = match input.char_indices().find(|&(_, x)| !predicate(x)) {
+        Some((i, _)) => i,
+        None => input.len(),
     };
+    println!("{}", index);
 
     // slice okay as index is guaranteed to be a valid character boundary
     (&input[index..], &input[..index])
@@ -15,6 +17,7 @@ where
 #[test]
 fn test_take_while() {
     assert_eq!(take_while("ab老 ", char::is_alphabetic), (" ", "ab老"));
+    assert_eq!(take_while("ab老", char::is_alphabetic), ("", "ab老"));
     assert_eq!(take_while(" abc", char::is_alphabetic), (" abc", ""));
     assert_eq!(take_while(" abc", char::is_whitespace), ("abc", " "));
 }
